5879b8119a6651a3d1bcab8fb7f713876aaf8edc
Ruaridh Macdonald <rmacd@mit.edu>
2025-07-29 06:39:22 -0400

    Fixed negation bug and tested on ThermalPower

--- src/model/assets/thermalpower.jl
+++ src/model/assets/thermalpower.jl
@@ -227,17 +227,22 @@ function make(asset_type::Type{ThermalPower}, data::AbstractDict{Symbol,Any}, sy
     @add_balance(
         thermal_transform,
         :fuel_consumption,
-        get(transform_data, :fuel_consumption, 1.0) * flow(elec_edge) == flow(fuel_edge)
+        flow(fuel_edge) + get(transform_data, :fuel_consumption, 1.0) * flow(elec_edge) == 0.0
     )
-    add_balance_data(
+    @add_balance(
         thermal_transform,
         :emissions,
-        Dict(
-            fuel_edge.id => get(transform_data, :emission_rate, 0.0),
-            co2_edge.id => 1.0,
-            elec_edge.id => 0.0,
-        )
+        get(transform_data, :emission_rate, 0.0) * flow(fuel_edge) + flow(co2_edge) == 0.0
     )
+    # add_balance_data(
+    #     thermal_transform,
+    #     :emissions,
+    #     Dict(
+    #         fuel_edge.id => get(transform_data, :emission_rate, 0.0),
+    #         co2_edge.id => 1.0,
+    #         elec_edge.id => 0.0,
+    #     )
+    # )
 
     return ThermalPower(id, thermal_transform, elec_edge, fuel_edge, co2_edge)
 end
