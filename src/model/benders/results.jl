f299cdeb741b5d1eb6fab402bcd411aece03ec92
lbonaldo <39280783+lbonaldo@users.noreply.github.com>
2026-04-13 21:06:10 -0400

    Refactor Benders results structure into BendersModel and BendersConvergence

--- src/model/benders/results.jl
+++ src/model/benders/results.jl
@@ -1,28 +1,32 @@
-struct BendersResults
-    planning_problem::Model
-    planning_sol::NamedTuple
-    subop_sol::Dict{Any, Any}
+"""Convergence diagnostics from a completed Benders solve."""
+struct BendersConvergence
     LB_hist::Vector{Float64}
     UB_hist::Vector{Float64}
     gap_hist::Vector{Float64}
     termination_status::AbstractString
     cpu_time::Vector{Float64}
-    planning_sol_hist::Matrix{Float64}
-    op_subproblem::Union{Vector{Dict{Any, Any}},DistributedArrays.DArray}
 end
 
-# Define constructor
-# BendersResults(nt::NamedTuple) = convert(BendersResults, nt)
-BendersResults(nt::NamedTuple, 
-    op_subproblem::Union{Vector{Dict{Any, Any}},DistributedArrays.DArray}
-) = BendersResults(nt.planning_problem, 
-    nt.planning_sol, 
-    nt.subop_sol,
-    nt.LB_hist, 
-    nt.UB_hist, 
-    nt.gap_hist,
-    nt.termination_status,
-    nt.cpu_time, 
-    nt.planning_sol_hist,
-    op_subproblem
+BendersConvergence(nt::NamedTuple) = BendersConvergence(
+    nt.LB_hist, nt.UB_hist, nt.gap_hist, nt.termination_status, nt.cpu_time
 )
+
+"""A container for a Benders problem. Mirrors JuMP's `Model` pattern:
+`generate_model` sets up the problem (including subproblem initialization),
+`optimize!` runs the Benders solve, extracts results, and stores them in the
+`planning_sol`, `subop_sol`, and `convergence` fields."""
+mutable struct BendersModel
+    settings::NamedTuple
+    update_target::Union{Case, System}
+    planning_problem::Model
+    subproblems::Union{Vector{Dict{Any, Any}}, DistributedArrays.DArray}
+    linking_variables_sub::Dict
+    planning_sol::Union{NamedTuple, Nothing}
+    subop_sol::Union{Dict{Any, Any}, Nothing}
+    convergence::Union{BendersConvergence, Nothing}
+end
+
+# Convenience constructor for BendersModel when planning_sol, subop_sol, and convergence are not yet available
+# they will be populated after optimize! is called
+BendersModel(settings, target, planning_problem, subproblems, linking_variables_sub) =
+    BendersModel(settings, target, planning_problem, subproblems, linking_variables_sub, nothing, nothing, nothing)
